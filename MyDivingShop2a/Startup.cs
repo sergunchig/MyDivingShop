add6bef0d4c65a7ee1bc378a84da0f1dd2460d50
Sergunchig <Sergunchig@gmail.com>
2018-09-24 22:29:36 +0300

    shop

---
+++ MyDivingShop2a/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyDivingShop2a.Startup))]
+namespace MyDivingShop2a
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
